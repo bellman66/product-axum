7c434aabb45e92ff06fd1597186e672f824abd0e
rndso15 <rndso15@naver.com>
2023-05-09 18:45:08 +0900

    feat : default axum setting feat : index status check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod domain;
+
+use std::net::SocketAddr;
+use axum::{Router, Server, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Check Server Status
+    println!("Server Status is Ok");
+
+    let router = Router::new().route("/", get(domain::index::get_status));
+
+    Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
